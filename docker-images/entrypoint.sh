8692259b54cebf75e5df322c7be8bf5fe14a36e2
hoeghh <heh@praqma.net>
2019-11-28 13:04:34 +0100

    Added secure option, both self signed and with trusted certs

--- docker-images/entrypoint.sh
+++ docker-images/entrypoint.sh
@@ -9,6 +9,15 @@ if [[ -z "${CODE_PORT}" ]]; then
     CODE_PORT=8080
 fi
 
+if [[ "${SECURE}" == "true" ]]; then
+  if [[ -z "${SSL_LOCATION}" ]]; then
+    SECURE_PARAM="--cert"
+  else
+    SECURE_PARAM="--cert $SSL_LOCATION"
+  fi
+fi
+
+
 sudo chown $(id -u):$(id -g) /var/run/docker.sock
 
-exec code-server --host=0.0.0.0 --port=${CODE_PORT} --auth=password --disable-telemetry
+exec code-server ${SECURE_PARAM} --host=0.0.0.0 --port=${CODE_PORT} --auth=password --disable-telemetry
