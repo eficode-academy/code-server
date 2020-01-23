73f4b23e9e1cd34c6a15db9edc5116f54b3c0fcb
Michael Frikke Madsen <mibias.madsen@gmail.com>
2020-01-23 12:28:18 +0100

    Update README, use praqma/dgoss

--- docker-images/test-with-dgoss.sh
+++ docker-images/test-with-dgoss.sh
@@ -5,4 +5,6 @@ docker run --rm \
     -v $PWD/goss_wait.yaml:/goss_wait.yaml \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e GOSS_FILES_STRATEGY=cp \
-    kiwicom/dgoss dgoss run -e SECURE=true praqma/vscode:tmp
+    praqma/dgoss:0.3.7 dgoss run  \
+        -e SECURE=true \
+        praqma/vscode:tmp
